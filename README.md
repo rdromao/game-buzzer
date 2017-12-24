6bad57e18413b26c3682ef5fc538c51943e5ae8a
Ricardo Romao <rdromao@gmail.com>
2017-12-24 18:02:59 -0400

    Adds settings to change amount of players and turn duration

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# game-buzzer
+# Game Buzzer
 Web based, same keyboard, game buzzer.
+
+<p align="center">
+  <a href="https://imgflip.com/gif/21n3lu"><img src="https://i.imgflip.com/21n3lu.gif" title="made at imgflip.com"/></a>
+</p>
 
 Just press "A", "H" or "L" and a color will lock/buzz so that team gets the turn (for 5 seconds).
 
-<p align="center">
-  <a href="https://imgflip.com/gif/21mwfl"><img src="https://i.imgflip.com/21mwfl.gif" title="made at imgflip.com"/></a>
-</p>
+You can also hit Escape to cancel a buzz.
+
+## Settings
+
+You can change the amount of players between 1 and 5.
+
+And you can change the turn duration between 1 and 10 seconds.
+
+
--- buzzer.js
+++ buzzer.js
@@ -1,57 +1,97 @@
+// Is it somebody's turn?
 var buzzed = false;
 
+// For how long the buzz will last
+var buzzduration = 5;
+
+// How many buzzers/teams are there
+var buzzers = 5;
+
+// Countdown timer global variable
 var countdown;
 
-function sleep(ms) {
- return new Promise(resolve => setTimeout(resolve, ms));
-}
 
+// After turn's time is up or when Escape is pressed:
 function killcountdown(){
-	clearTimeout(countdown);
+	clearInterval(countdown)
   resetBuzzer();
 }
 
-// Reset buzzer colors to initial state
+// Reset buzzer colors and styles to initial state
 function resetBuzzer(){
-  $('.left').css("width", "33.33%");
-  $('.middle').css("width", "33.33%");
-  $('.right').css("width", "33.33%");
-	$('.left').css("background-color", "#FFCDD2");
-	$('.middle').css("background-color", "#BBDEFB");
-	$('.right').css("background-color", "#C8E6C9");
-  $('.press-key-indicator').show();
+
+  $('.buzzer').css('width', (100/buzzers) +'%' );
+  $('.buzzer').css('opacity', '.5');
+  $('.leftmost .press-key-indicator').html('Press "A"');
+  $('.left .press-key-indicator').html('Press "D"');
+  $('.middle .press-key-indicator').html('Press "G"');
+  $('.right .press-key-indicator').html('Press "J"');
+  $('.rightmost .press-key-indicator').html('Press "L"');
+  
+  $('.press-key-indicator').removeClass('active');
+  
   buzzed = false;
 }
 
+// Update seconds in buzzer as they pass and then reset
+function countDown(i) {
+    countdown = setInterval(
+      function () {
+        $('.press-key-indicator.active').html(i);
+        i-- || killcountdown();  //if i is 0, then stop the countdown
+      }, 1000);
+}
+
+// Buzz a buzzer
+function buzz(classBuzzed){
+
+  buzzed = true;
+
+  $('.buzzer:not('+classBuzzed+')').css('width', (4/(buzzers-1)) +'%' );
+
+  $(classBuzzed).css('opacity','1');
+  $(classBuzzed).css('width', '96%');
+  $(classBuzzed+' .press-key-indicator').addClass('active');
+  $(classBuzzed+' .press-key-indicator').html(buzzduration);
+
+  countDown(buzzduration-1);
+}
+
+// Listen for key presses and lock when a valid one is pressed
 window.onkeyup = function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
     
     if (key == 65 && !buzzed) {
-    		buzzed = true;
-        $('.left').css("background-color", "#F44336");
-        $('.left').css("width", "98%");
-        $('.middle').css("width", "1%");
-        $('.right').css("width", "1%");
-        $('.press-key-indicator').hide();
-        countdown = setTimeout(resetBuzzer, 5000);
-    }else if (key == 72 && !buzzed) {
-    		buzzed = true;
-        $('.middle').css("background-color", "#2196F3");
-        $('.left').css("width", "1%");
-        $('.middle').css("width", "98%");
-        $('.right').css("width", "1%");
-        $('.press-key-indicator').hide();
-        countdown = setTimeout(resetBuzzer, 5000);
-    }else if (key == 76 && !buzzed) {
-    		buzzed = true;
-        $('.right').css("background-color", "#4CAF50");
-        $('.left').css("width", "1%");
-        $('.middle').css("width", "1%");
-        $('.right').css("width", "98%");
-        $('.press-key-indicator').hide();
-        countdown = setTimeout(resetBuzzer, 5000);
+        buzz('.leftmost');
+    }else if (key == 68 && !buzzed && buzzers > 1) {
+        buzz('.left');
+    }else if (key == 71 && !buzzed && buzzers > 2) {
+    		buzz('.middle');
+    }else if (key == 74 && !buzzed && buzzers > 3) {
+    		buzz('.right');
+    }else if (key == 76 && !buzzed && buzzers > 4) {
+        buzz('.rightmost');
     }else if (key == 27 && buzzed){
     		killcountdown();
     }
     
 }
+
+// Show/hide settings list
+$(".settings .clickable").on('click', function(){
+  $(".settings-list-container").slideToggle(150, function(){
+    $('.settings').hide().show(0);
+  });
+});
+
+// When buzzers duration changes, save the setting change
+$("input[name=buzz-duration]").bind('keyup mouseup', function () {
+    buzzduration = $(this).val();            
+});
+
+// When buzzers amount changes, save the setting change and apply it
+$("input[name=buzzer-amount]").bind('keyup mouseup', function () {
+    buzzers = $(this).val();
+
+    resetBuzzer();
+});
